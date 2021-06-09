71f9dd5b6ddc30ff911242488c7530f79a39a588
jay719 <javariab17@gmail.com>
2021-06-08 22:43:16 -0600

    added a navigate function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import { useHistory } from 'react-router';
 
 export default function Header () {
+    const history = useHistory();
 
+    const navigate = (text) => {
+        console.log(text)
+      history.push(text);
+    }
     return (
         <div className="Header">
             <div className="App-header">
-              <div>
-                <p>Javaria Brascom</p>
-              </div>
-              <div className="Pages"> 
-                <a className="link" href="/">Home</a>
-                <a className="link" href="/projects">Projects</a>
-              </div>
+                <div>
+                    <p>Javaria Brascom</p>
+                </div>
+                <div className="Pages"> 
+                    <div className="nav-btn"><h7 onClick={navigate('/')}>HOME</h7></div>
+                    <div className="nav-btn"><h7 onClick={navigate('/portfolio')}>PORTFOLIO</h7></div>
+                </div>
             </div>
         </div>
     )
