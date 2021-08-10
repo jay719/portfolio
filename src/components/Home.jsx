3a43d0e47178bf91e3b72cf36f6328cb9e4d8ef2
jay719 <javariab17@gmail.com>
2021-08-09 23:44:41 -0600

    added changed more files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,7 @@
 import React, { useState } from 'react'
 
 export default function Home() {
-    // const [numCounter, setNumCounter] = useState(0)
-    // const handleClick = () => {
-    //     setNumCounter(numCounter + 1)
-    // }  
 
-    {/* <div>{numCounter}</div>
-            
-            <button onClick={handleClick}>count</button> */}
     return (
         <div className="Home">
             
