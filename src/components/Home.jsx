59fc3396933c4d455a82d3b0b02527d7183c2ec6
jay719 <javariab17@gmail.com>
2021-08-09 23:59:55 -0600

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 export default function Home() {
 
     return (
-        <div className="Home">
+        <div className="home">
             
         </div>
     )
