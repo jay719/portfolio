6053f08447dfe82cc4e342288ac12d4add58ceb7
jay719 <javariab17@gmail.com>
2021-08-10 21:04:00 -0600

    added ityped npm package

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,18 +4,23 @@ export default function Home() {
 
     const titles = ["Full Stack Developer", "Inovator", "Scholar"]
 
+    const typingText = () => {
+        titles.map(title => {
+            return <h2>{title}</h2>
+        })
+    }
     return (
 
         
         <div className="home" id="home">
             <div className="left">
-                
+
             </div>
             <div className="right">
                 <div className="name">
                     <h3>Javaria Brascom</h3>
                     <div className="typing-text">
-                
+                        {typingText()}
                     </div>
                 </div>
             
