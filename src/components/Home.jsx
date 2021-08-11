fa8c8e3f1b6bac2f64ef62b40d6972c95d690e14
jay719 <javariab17@gmail.com>
2021-08-10 20:49:49 -0600

    added text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,24 @@ import React, { useState } from 'react'
 import '../styling/home.scss'
 export default function Home() {
 
+    const titles = ["Full Stack Developer", "Inovator", "Scholar"]
+
     return (
+
+        
         <div className="home" id="home">
+            <div className="left">
+                
+            </div>
+            <div className="right">
+                <div className="name">
+                    <h3>Javaria Brascom</h3>
+                    <div className="typing-text">
+                
+                    </div>
+                </div>
+            
+            </div>
         </div>
     )
 }
